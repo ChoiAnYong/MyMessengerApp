855198fdba9ac1bb80b7a672898a8e8917a294c4
ChoiAnYong <dksdyd78@naver.com>
2024-03-11 15:08:30 +0900

    feat: 프로젝트 세팅

---
+++ MyMessenger/MyMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMessengerApp.swift
+//  MyMessenger
+//
+//  Created by 최안용 on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
